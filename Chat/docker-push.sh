f9140af4e38e1f771624628409c6cdb08a66452b
ThreadGit <ThreadEmail@163.com>
2023-11-11 20:41:06 +0800

    增加dockerfile

---
+++ Chat/docker-push.sh
@@ -0,0 +1,11 @@
+# 处理参数
+tag=$1
+if [ -z $1 ]; then
+  tag=latest
+fi
+
+# 构建镜像 --platform linux/amd64 设为x86架构
+docker build --platform linux/amd64 -t registry.cn-hangzhou.aliyuncs.com/thread-docker-hub/ikun-chat:$tag .
+
+# 推送镜像
+docker push registry.cn-hangzhou.aliyuncs.com/thread-docker-hub/ikun-chat:$tag
---
+++ Chat/docker-start.sh
@@ -0,0 +1,24 @@
+# 拉取最新镜像
+docker pull registry.cn-hangzhou.aliyuncs.com/thread-docker-hub/ikun-chat:latest
+
+# 干掉运行的容器
+docker rm -f ikun-chat
+
+# sqllite
+dbfile=$1
+
+# 设置变量默认值
+# /Users/thread/workspaces/GitHubRepository/IkunChat/Chat/src/main/resources/db
+# /root/ikun-chat/db
+if [ -z $1 ]; then
+  dbfile=/root/ikun-chat/db
+fi
+
+echo sqllite：$dbfile
+
+# 运行容器
+docker run --name ikun-chat \
+           -d \
+           -v $dbfile:/ikun-chat/db \
+           -p 777:777 -p 8888:8888 \
+       registry.cn-hangzhou.aliyuncs.com/thread-docker-hub/ikun-chat
